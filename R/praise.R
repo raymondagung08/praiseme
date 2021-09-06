bacb458f7772e2de959e205bf7c7839f03d74356
Raymond Agung <raymond.agung@students.mq.edu.au>
2021-09-06 21:52:30 +0700

    Initial commit for the praiseme package

---
+++ R/praise.R
@@ -0,0 +1,21 @@
+#' Deliver praise
+#'
+#' @description this function is super useful when you're feeling sad
+#' and is useful for everyone.
+#'
+#' @param name text string; This is the name of the person I want to praise.
+#' @param punctuation text string; This is our emphasis as a text input
+#'
+#' @return Text string with praise
+#' @export
+#'
+#' @examples
+#' praise(name = "Steph", punctuation = "!")
+praise <- function(name, punctuation = "!"){
+  glue::glue("You're the best, {name}{punctuation}")
+}
+praise("Thomas")
+?praise
+usethis::use_package("glue")
+usethis::use_testthat()
+usethis::use_test("praise")
